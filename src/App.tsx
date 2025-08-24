8003bf7c1103e8736df71ec499d55184bf81ba35
あなたの名前 <atsumi.sohshiroh@gmail.com>
2025-08-24 21:04:48 +0900

    親の回答待機画面の作製

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,8 @@ import TopMenu from './TopMenu';
 import SubPage from './SubPage';
 import Standby from './Standby';
 import ParentToppickPage from './ParentTopicPage';
+import ParentWaiting from './ParentWaiting';
+
 function App() {
   return (
     <Router>
@@ -12,6 +14,7 @@ function App() {
         <Route path="/sub" element={<SubPage />} />
         <Route path="/standby" element={<Standby />} />
         <Route path="/parenttopick" element={<ParentToppickPage />} />
+        <Route path="/parentwaiting" element={<ParentWaiting />} />
         
       </Routes>
     </Router>
